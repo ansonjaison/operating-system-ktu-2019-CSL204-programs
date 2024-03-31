84f63b0397fcbc17ae5bab4086fd1acae7fca11b
Anson Jaison <84223694+ansonjaison@users.noreply.github.com>
2024-03-31 16:31:38 +0530

    Code change with output

--- firstfit.c
+++ firstfit.c
@@ -12,7 +12,7 @@ struct block
 
 void main()
 {
-    int n, m, i, j, rem;
+    int n, m, i, j;
 
     printf("\nEnter the number of Memory Blocks:\t");
     scanf("%d", &m);
@@ -40,36 +40,78 @@ void main()
     i = 0;
     j = 0;
 
-    //first fit  i -> process  j -> block
-   while (i < n)
-{
-    if (p[i].alloc == 0)
-    {
-        for (j = 0; j < m; j++)
-        {
-            if (p[i].psize <= b[j].bsize)
-            {
-                p[i].alloc = 1;
-                p[i].bnum = b[j].bnum;
-                p[i].bx = j;
-                // Update the remaining free space in the block
-                b[j].bsize -= p[i].psize;
-                break;
-            }
-        }
-    }
-    i++;
-}
     printf("\n-------------------------------------------------------------------");
     printf("\nPROCESS ID\tPROCESS SIZE\tBLOCK NUMBER\tFREE SPACE\n");
     printf("\n-------------------------------------------------------------------");
 
+    // First fit: i -> process, j -> block
+    while (i < n)
+    {
+        if (p[i].alloc == 0)
+        {
+            for (j = 0; j < m; j++)
+            {
+                if (p[i].psize <= b[j].bsize)
+                {
+                    p[i].alloc = 1;
+                    p[i].bnum = b[j].bnum;
+                    p[i].bx = j;
+                    // Update the remaining free space in the block before allocation
+                    int free_space = b[j].bsize - p[i].psize;
+                    printf("\n%d\t\t%d\t\t%d\t\t%d", p[i].pid, p[i].psize, p[i].bnum, free_space);
+                    b[j].bsize -= p[i].psize;
+                    break;
+                }
+            }
+        }
+        i++;
+    }
+
+    // Printing processes not allocated
     for (i = 0; i < n; i++)
     {
-        if (p[i].alloc == 1)
-            printf("\n%d\t\t%d\t\t%d\t\t%d", p[i].pid, p[i].psize, p[i].bnum, b[p[i].bx].bsize);
-        else
+        if (p[i].alloc == 0)
             printf("\n%d\t\t%d\t\t-\t\t-\t\t [NOT ALLOCATED]", p[i].pid, p[i].psize);
     }
-    printf("\n");
+    printf("\n-------------------------------------------------------------------\n");
 }
+
+/* OUTPUT
+
+Enter the number of Memory Blocks:      3
+Memory Block 1 :
+        Enter the Block Number: 1
+        Enter the Block Size:   400
+Memory Block 2 :
+        Enter the Block Number: 2
+        Enter the Block Size:   300
+Memory Block 3 :
+        Enter the Block Number: 3
+        Enter the Block Size:   500
+
+Enter the number of processes:  4
+Process 1 :
+        Enter the Process Id:   1
+        Enter the Process Size: 200
+Process 2 :
+        Enter the Process Id:   2
+        Enter the Process Size: 350
+Process 3 :
+        Enter the Process Id:   3
+        Enter the Process Size: 150
+Process 4 :
+        Enter the Process Id:   4
+        Enter the Process Size: 400
+
+-------------------------------------------------------------------
+PROCESS ID      PROCESS SIZE    BLOCK NUMBER    FREE SPACE
+
+-------------------------------------------------------------------
+1               200             1               200
+2               350             3               150
+3               150             1               50
+4               400             -               -                [NOT ALLOCATED]
+-------------------------------------------------------------------
+
+ */   
+    
