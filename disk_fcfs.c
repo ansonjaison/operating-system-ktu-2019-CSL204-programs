f4992dc97a1e55a2c5ac0411c3335653f5f48f51
Anson Jaison <84223694+ansonjaison@users.noreply.github.com>
2024-05-13 17:28:32 +0530

    Update disk_fcfs.c

--- disk_fcfs.c
+++ disk_fcfs.c
@@ -1,29 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int seeks = 0, req[30];
 
 void main() {
     int i, n, head, cylinders;
 
-    printf("Enter the number of Requests: ");
+    printf("\nEnter the number of Requests: ");
     scanf("%d", &n);
 
-    printf("Enter the number of Cylinders: ");
+    printf("\nEnter the number of Cylinders: ");
     scanf("%d", &cylinders);
 
-    printf("Enter the initial position of head: ");
+    printf("\nEnter the initial position of head: ");
     scanf("%d", &head);
 
-    printf("Enter the Requests: ");
-    for (i = 0; i < n; i++)
+    printf("\nEnter the Requests: ");
+    for(i = 0; i < n; i++)
         scanf("%d", &req[i]);
 
     seeks += abs(req[0] - head);
 
-    for (i = 1; i < n; i++) {
+    for(i = 1; i < n; i++) {
         seeks += abs(req[i] - req[i - 1]);
     }
 
-    printf("Total No. of Seeks = %d", seeks);
+    printf("\nTotal No. of Seeks = %d", seeks);
 }
+
+
+/* OUTPUT
+
+Enter the number of Requests: 7
+
+Enter the number of Cylinders: 200
+
+Enter the initial position of head: 55
+
+Enter the Requests: 93 176 42 148 27 14 180
+
+Total No. of Seeks = 661
+
+*/
