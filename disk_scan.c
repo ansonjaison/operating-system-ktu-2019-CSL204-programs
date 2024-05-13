43c19799b2c9e7091e4687b3a337f5ba2cda46a7
Anson Jaison <84223694+ansonjaison@users.noreply.github.com>
2024-05-13 17:28:56 +0530

    Update disk_scan.c

--- disk_scan.c
+++ disk_scan.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int seeks = 0, req[30], order[30];
 
 void main() {
     int i, j, n, head, cylinders, least, temp, pos;
 
-    printf("Enter the number of Requests: ");
+    printf("\nEnter the number of Requests: ");
     scanf("%d", &n);
 
-    printf("Enter the number of Cylinders: ");
+    printf("\nEnter the number of Cylinders: ");
     scanf("%d", &cylinders);
 
-    printf("Enter the initial position of head: ");
+    printf("\nEnter the initial position of head: ");
     scanf("%d", &head);
 
-    printf("Enter the Requests: ");
-    for (i = 0; i < n; i++)
+    printf("\nEnter the Requests: ");
+    for(i = 0; i < n; i++)
         scanf("%d", &req[i]);
 
     least = req[0];
-    for (i = 0; i < n; i++) {
-        if (least > req[i])
+    for(i = 0; i < n; i++) {
+        if(least > req[i])
             least = req[i];
     }
 
     seeks = (cylinders - least - 1) + (cylinders - head - 1);
 
-    for (i = 0; i < n - 1; i++) {
-        for (j = i + 1; j < n; j++) {
-            if (req[i] > req[j]) {
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(req[i] > req[j]) {
                 temp = req[j];
                 req[j] = req[i];
                 req[i] = temp;
@@ -38,31 +38,35 @@ void main() {
     }
 
     printf("Order of Execution: ");
-    for (i = 0; i < n; i++) {
-        if (head < req[i]) {
+    for(i = 0; i < n; i++) {
+        if(head < req[i]) {
             pos = i;
             break;
         }
     }
 
-    for (i = pos; i < n; i++) {
-        printf("%d\t", req[i]);
+    for(i = pos; i < n; i++) {
+        printf("%d ", req[i]);
     }
 
-    for (i = pos - 1; i >= 0; i--) {
-        printf("%d\t", req[i]);
+    for(i = pos - 1; i >= 0; i--) {
+        printf("%d ", req[i]);
     }
 
-    printf("\nTotal No. of Seeks: %d\n", seeks);
+    printf("\nTotal No. of Seeks = %d", seeks);
 }
+
 
 /* OUTPUT
 
 Enter the number of Requests: 7
-Enter the number of Cylinders: 90
-Enter the initial position of head: 53
-Enter the Requests:  12 34 52 14 25 68 39
-Order of Execution: 68  52      39      34      25      14      12
-Total No. of Seeks: 113
+
+Enter the number of Cylinders: 200
+
+Enter the initial position of head: 55
+
+Enter the Requests: 93 176 42 48 27 14 180
+Order of Execution: 93 176 180 48 42 27 14
+Total No. of Seeks = 329
 
 */
