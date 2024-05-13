563cb31cfab013f7db10f3658c78eec1f960ee8d
Anson Jaison <84223694+ansonjaison@users.noreply.github.com>
2024-05-13 17:30:28 +0530

    Update producerconsumer_semaphore.c

--- producerconsumer_semaphore.c
+++ producerconsumer_semaphore.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int mutex = 1, full = 0, empty, x = 0;
 
@@ -23,22 +23,22 @@ void consumer() {
 
 void main() {
     int ch;
-    printf("Enter Buffer size:\t ");
+    printf("Enter Buffer size: ");
     scanf("%d", &empty);
     printf("\nChoices:\n1. Producer\t2. Consumer\t3. Exit");
-    while (1) {
-        printf("\n\nEnter Choice \t:");
+    while(1) {
+        printf("\n\nEnter Choice: ");
         scanf("%d", &ch);
-        switch (ch) {
+        switch(ch) {
             case 1:
-                if (mutex == 1 && empty != 0)
+                if(mutex == 1 && empty != 0)
                     producer();
                 else
                     printf("Buffer is Full");
                 break;
 
             case 2:
-                if (mutex == 1 && full != 0)
+                if(mutex == 1 && full != 0)
                     consumer();
                 else
                     printf("Buffer is Empty");
@@ -49,3 +49,31 @@ void main() {
         }
     }
 }
+
+
+/* OUTPUT
+
+Enter Buffer size: 2
+
+Choices:
+1. Producer     2. Consumer     3. Exit
+
+Enter Choice: 1
+Producer produces 1
+
+Enter Choice: 1
+Producer produces 2
+
+Enter Choice: 1
+Buffer is Full
+
+Enter Choice: 2
+Consumer consumes 2
+
+Enter Choice: 2
+Consumer consumes 1
+
+Enter Choice: 2
+Buffer is Empty
+
+*/
