92ee595594f18af9f5be8f8483ff7490e73551bd
Anson Jaison <84223694+ansonjaison@users.noreply.github.com>
2024-07-21 19:28:35 +0530

    Update disk_cscan.c

--- disk_cscan.c
+++ disk_cscan.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int seeks = 0, req[30], order[30];
+int seeks = 0, req[30];
 
 void main() {
-    int i, j, n, head, cylinders, least, temp, pos;
+    int i, j, n, head, cylinders, temp, pos;
 
     printf("\nEnter the number of Requests: ");
     scanf("%d", &n);
@@ -19,43 +19,31 @@ void main() {
     for(i = 0; i < n; i++)
         scanf("%d", &req[i]);
 
-    least = req[0];
-    for(i = 0; i < n; i++) {
-        if(least > req[i])
-            least = req[i];
-    }
-
-    for(i = 0; i < n - 1; i++) {
-        for(j = i + 1; j < n; j++) {
-            if(req[i] > req[j]) {
-                temp = req[j];
-                req[j] = req[i];
-                req[i] = temp;
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j<n;j++){
+            if(req[i]>req[j]){
+                temp=req[j];
+                req[j]=req[i];
+                req[i]=temp;
             }
         }
     }
-
-    printf("Order of Execution: ");
-    for(i = 0; i < n; i++) {
-        if(head < req[i]) {
-            pos = i;
+    for(i=0;i<n;i++){
+        if(req[i]>head){
+            pos=i;
             break;
         }
     }
-
-    seeks = (cylinders - head - 1) + (cylinders - 1) + (req[pos - 1]);
-
-    for(i = pos; i < n; i++) {
+    printf("\nOrder of execution is: ");
+    for(i=pos;i<n;i++){
         printf("%d ", req[i]);
     }
-
-    for(i = 0; i <= pos - 1; i++) {
+    for(i=0;i<=pos-1;i++){
         printf("%d ", req[i]);
     }
-
-    printf("\nTotal No. of Seeks = %d", seeks);
+    seeks = (cylinders-1-head) + (cylinders-1-0) + (req[pos-1]-0);
+    printf("\nTotal no.of seeks: %d", seeks);
 }
-
 
 /* OUTPUT
 
@@ -66,7 +54,8 @@ Enter the number of Cylinders: 200
 Enter the initial position of head: 55
 
 Enter the Requests: 93 176 42 148 27 14 180
-Order of Execution: 93 148 176 180 14 27 42
-Total No. of Seeks = 385
+
+Order of execution is: 93 148 176 180 14 27 42
+Total no.of seeks: 385
 
 */
